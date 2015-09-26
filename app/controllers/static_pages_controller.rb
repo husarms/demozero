85e2b06d4d8cd8f64023d576b699c2376d488214
husarms <husarms@gmail.com>
2015-09-26 14:25:47 -0400

    Added demo and contact pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+  def audio_visualizer
+  end
+  def demos
+  end
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'static_pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'static_pages#home'
+  get 'audio_visualizer' => 'static_pages#audio_visualizer'
+  get 'demos' => 'static_pages#demos'
+  get 'contact' => 'static_pages#contact'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
